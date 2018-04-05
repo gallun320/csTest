ef721471e80d87408c02d844f83193bcab4fe5f3
galklun320@gmail.com <galklun320@gmail.com>
2018-04-05 17:25:58 +0300

    Add delegate

--- Program.cs
+++ Program.cs
@@ -9,7 +9,14 @@ namespace csTest
 {
     class Program
     {
-        public static List<string> reqDataCollection = new List<string>() { }; 
+        private static List<string> reqDataCollection = new List<string>() { }; 
+        private delegate string ReqMethodDelegate(string reqUrl, HttpListenerRequest request);
+        private static Dictionary<string, ReqMethodDelegate> _methods = new Dictionary<string, ReqMethodDelegate>() {
+            {"GET", GetRequestWorker},
+            {"POST", PostRequestWorker},
+            {"DELETE", DeleteRequestWorker},
+            {"PUT", PutRequestWorker}
+        };
         static void Main(string[] args)
         {
             //GetRequestMethod();
@@ -64,62 +71,79 @@ namespace csTest
         {
             var reqMethod = request.HttpMethod;
             var reqUrl = request.Url.OriginalString;
-            var result = "";
 
+            if(!_methods.ContainsKey(reqMethod))
+                return "<html><head><meta charset='utf8'></head><body>Привет мир!</body></html>";
 
-            switch(reqMethod)
-            {
-                case "GET":
-                    var reqGetParams = reqUrl.Split('/');
-                    if(Array.IndexOf(reqGetParams, "items") > -1) 
-                    {
-                        result = String.Join(", ", reqDataCollection.ToArray());    
-                    } 
-                    else if(Array.IndexOf(reqGetParams, "item") > -1) 
-                    {
-                        var reqGetDataIndex = Int32.Parse(reqGetParams[reqGetParams.Length - 1]);
-                        var reqEl = reqDataCollection[reqGetDataIndex];
-                        result = reqEl;
-
-                    }
-                    break;
-                case "POST":
-                    var data = "";
-                    using ( var stream = new StreamReader(request.InputStream, Encoding.UTF8))
-                    {
-                        data = stream.ReadToEnd();
-                    }
-
-
-                    reqDataCollection.Add(data);
-                    result = "OK POST";
-                    break;
-                case "DELETE":
-                    var reqDeleteParams = reqUrl.Split('/');
-                    var reqDeleteDataIndex = Int32.Parse(reqDeleteParams[reqDeleteParams.Length - 1]);
-                    if(reqDeleteDataIndex >= 9999) 
-                    {
-                        reqDataCollection.Clear();
-                    }
-                    var reqDeletEl = reqDataCollection.Remove(reqDataCollection[reqDeleteDataIndex]);
-                    result = "OK DELETE";
-                    break;
-                case "PUT":
-                    var dataPut = "";
-                    var reqPutParams = reqUrl.Split('/');
-                    var reqPutDataIndex = Int32.Parse(reqPutParams[reqPutParams.Length - 1]);
-                    using (var stream = new StreamReader(request.InputStream, Encoding.UTF8))
-                    {
-                        dataPut = stream.ReadToEnd();
-                    }
-                    reqDataCollection[reqPutDataIndex] = dataPut;
-                    result = "OK PUT";
-                    break;
-            }
+            var result = _methods[reqMethod](reqUrl, request);
 
             var responseString = String.Format("<html><head><meta charset='utf8'></head><body>Привет мир! {0}</body></html>", result);
 
             return responseString;
+        }
+
+        public static string GetRequestWorker(string reqUrl, HttpListenerRequest request)
+        {
+
+            var result = "";
+            var reqGetParams = reqUrl.Split('/');
+            if(Array.IndexOf(reqGetParams, "items") > -1) 
+            {
+                result = String.Join(", ", reqDataCollection.ToArray());    
+            } 
+            else if(Array.IndexOf(reqGetParams, "item") > -1) 
+            {
+                var reqGetDataIndex = Int32.Parse(reqGetParams[reqGetParams.Length - 1]);
+                var reqEl = reqDataCollection[reqGetDataIndex];
+                result = reqEl;
+
+            }
+
+            return result;
+        }
+
+        public static string PostRequestWorker(string reqUrl, HttpListenerRequest request)
+        {
+            var result = "";
+            var data = "";
+            using ( var stream = new StreamReader(request.InputStream, Encoding.UTF8))
+            {
+                data = stream.ReadToEnd();
+            }
+
+
+            reqDataCollection.Add(data);
+            result = "OK POST";
+            return result;
+        }
+
+        public static string DeleteRequestWorker(string reqUrl, HttpListenerRequest request)
+        {
+            var result = "";
+            var reqDeleteParams = reqUrl.Split('/');
+            var reqDeleteDataIndex = Int32.Parse(reqDeleteParams[reqDeleteParams.Length - 1]);
+            if(reqDeleteDataIndex >= 9999) 
+            {
+                reqDataCollection.Clear();
+            }
+            var reqDeletEl = reqDataCollection.Remove(reqDataCollection[reqDeleteDataIndex]);
+            result = "OK DELETE";
+            return result;
+        }
+
+        public static string PutRequestWorker(string reqUrl, HttpListenerRequest request)
+        {
+            var result = "";
+            var dataPut = "";
+            var reqPutParams = reqUrl.Split('/');
+            var reqPutDataIndex = Int32.Parse(reqPutParams[reqPutParams.Length - 1]);
+            using (var stream = new StreamReader(request.InputStream, Encoding.UTF8))
+            {
+                dataPut = stream.ReadToEnd();
+            }
+            reqDataCollection[reqPutDataIndex] = dataPut;
+            result = "OK PUT";
+            return result;
         }
     }
 }
